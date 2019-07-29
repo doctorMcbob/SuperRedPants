ce86538e951b58ccb043d9e7a2d7cb4133cf2bf9
doctormcbob <wootenwesley@gmail.com>
2019-07-29 06:36:23 -0700

    Completed TAS with a time of 2:13

--- TAS.py
+++ TAS.py
@@ -1,12 +1,13 @@
 #TAS.py
 """
-Game mode for making and playing TASes, should work, needs testing
+Still gets desyncs, though nowhere near as often...
 """
 import pygame
 from pygame.locals import *
 import platformer as plt
 from copy import deepcopy
 import pickle
+import os
 
 HEL12 = pygame.font.SysFont("Helvetica", 12)
 
@@ -61,7 +62,17 @@ def play_tas():
         plt.fill_back(plt.levels[plt.level])
         plt.draw_level(plt.levels[plt.level])
         pygame.display.update()
-        
+    plt.flag = 1
+    while plt.flag:
+        plt.clock.tick(30)
+        plt.fill_back(plt.levels[plt.level])
+        plt.screen.blit(plt.endframe, (0, 0))
+        plt.screen.blit(plt.HEL32.render(str(plt.IGT // 60000) +":"+ ("0" + str(plt.IGT // 1000 % 60))[-2:], 0, (0, 0, 0)),(180, 160))
+        pygame.display.update()
+        for e in pygame.event.get():
+            if e.type == QUIT or e.type == KEYDOWN and e.key == K_ESCAPE: quit()
+            elif e.type == KEYDOWN and e.key == K_SPACE and plt.count >= 30: flag = False
+
 def new_frame():
     global events
     plt.g['count'] += 1
@@ -69,7 +80,7 @@ def new_frame():
     plt.evaluate_input(plt.g['count'], bouncer(events.copy()), keysfunc=bouncer(keymaster.copy()))
     plt.move_actors(plt.levels[plt.level], plt.g['count'])
     plt.move_player(plt.levels[plt.level])
-    statelist.append((plt.g.copy(), deepcopy(plt.levels[plt.level])))
+    statelist.append((deepcopy(plt.g), deepcopy(plt.levels[plt.level])))
     eventlist.append(events)
     keyslist.append(keymaster.copy())
     events = []
@@ -103,11 +114,52 @@ def make_event(type=KEYDOWN):
                 events.append(Event(e.key, type=type))
                 return
 
+def flush():
+    global frame, statelist
+    statelist = []
+    plt.level = 0
+    frame = 0
+    plt.level_title(plt.levels[plt.level])
+    plt.g = plt.reset()
+    plt.flag = True
+    plt.IGT = 0
+    plt.clock.tick()
+    while plt.flag:
+        statelist.append((deepcopy(plt.g), deepcopy(plt.levels[plt.level])))
+        frame += 1
+        if frame >= len(eventlist):
+            frame -= 1
+            break
+        plt.g['count'] += 1
+        plt.IGT += plt.clock.tick(30)
+        plt.evaluate_input(plt.g['count'], eventfunc=eventfunc, keysfunc=keysfunc)
+        plt.adjust_scroll()
+        plt.move_actors(plt.levels[plt.level], plt.g['count'])
+        plt.move_player(plt.levels[plt.level])
+        plt.fill_back(plt.levels[plt.level])
+        plt.draw_level(plt.levels[plt.level])
+        pygame.display.update()
+
+
 def foo(statelist):
     for i, state in enumerate(statelist):
         print("Frame", i, "Count", state[0]['count'])
 
-select_frame(frame)
+def find_desync(statelist):
+    c = 0
+    for frame, state in enumerate(statelist):
+        if state[0]['count'] != c+1: 
+            if state[0]['count'] == 0: print("new level ", frame)
+            else: print("desync frame ", frame)
+        c = state[0]['count']
+
+plt.level = 0
+frame = 0
+plt.level_title(plt.levels[plt.level])
+plt.g = plt.reset()
+statelist.append((deepcopy(plt.g), deepcopy(plt.levels[plt.level])))
+eventlist.append(events)
+keyslist.append(keymaster.copy())
 while True:
     pygame.display.update()
     for e in pygame.event.get():
@@ -119,9 +171,18 @@ while True:
             if e.key == K_b: frame -= 1 # back
             if e.key in [K_n, K_b]: select_frame(frame)
 
+            if e.key == K_j:
+
+                try:
+                    frame = int(input("Jump to frame : "))
+                    select_frame(frame)
+                except TypeError: pass
+
             if e.key == K_w: # what?
                 print(plt.g)
                 foo(statelist)
+            if e.key == K_SLASH:
+                find_desync(statelist)
             if e.key == K_c: # clear
                 statelist = statelist[:frame]
                 eventlist = eventlist[:frame]
@@ -134,19 +195,23 @@ while True:
             if e.key in keymaster:  # key toggle
                 keymaster[e.key] = False if keymaster[e.key] else True
             if e.key == K_p: play_tas() # play
+            if e.key == K_o: flush()
 
             if e.key == K_RETURN:
-                filename = input("Save as (blank for TAS): ")
-                if not filename: filename = "TAS"
-                with open(filename, "wb+") as file:
-                    pickle.dump((eventlist, keyslist, statelist), file)
+                filename = input("Save as (blank to cancel): ")
+                if os.path.isfile(filename):
+                    if input("Overwrite? Are you sure?") in ["N", "n", "no"]:
+                        filename = None
+                if filename:        
+                    with open(filename, "wb+") as file:
+                        pickle.dump((eventlist, keyslist, statelist), file)
 
             if e.key == K_TAB:
-                filename = input("Load from (blank for TAS): ")
-                if not filename: filename = "TAS"
-                with open(filename, "rb") as file:
-                    eventlist, keyslist, statelist = pickle.load(file)
-                    frame = 0
+                filename = input("Load from (blank for cancel): ")
+                if filename:
+                    with open(filename, "rb") as file:
+                        eventlist, keyslist, statelist = pickle.load(file)
+                        frame = 0
                     select_frame(frame)
 
     screen.blit(get_tastools(), (0, 0))
